02ce3286e80d6240151de11bdcb79cca79fab3d6
KavinprabhuK <kavinprabhu.kumaravel@lntinfotech.com>
2021-11-16 21:02:33 +0530

    initial commit

---
+++ src/main/java/com/honda/am/cqp/Application.java
@@ -0,0 +1,23 @@
+package com.honda.am.cqp;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.honda.am.cqp.util.LoggingUtil;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) throws IOException, SQLException {
+
+		LoggingUtil logging = new LoggingUtil();
+		logging.createLogFiles();
+		logging.moveOldLogFilesToArchive();
+		logging.deleteOldLogFiles();
+		System.out.println();
+		SpringApplication.run(Application.class, args);
+	}
+}
